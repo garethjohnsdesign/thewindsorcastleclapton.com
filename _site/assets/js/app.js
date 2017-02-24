f79572eb74abfec2a238eb80a8e3c3a9d2572704
Gareth Johns <info@garethjohnsdesign.com>
2017-02-24 19:46:53 +0000

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,15 +1,27 @@
 $(document).foundation();
 
-$('.gallery').flickity({
+$('.off-canvas a').on('click', function() {
+    $('.off-canvas').foundation('close');
+});
+
+$('.carousel--food').flickity({
+cellAlign: 'left',
+wrapAround: true,
 pageDots: true,
 imagesLoaded: true,
 percentPosition: false,
 arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
 
-$('.off-canvas a').on('click', function() {
-    $('.off-canvas').foundation('close');
+$('.carousel--drinks').flickity({
+cellAlign: 'left',
+wrapAround: true,
+pageDots: true,
+imagesLoaded: true,
+percentPosition: false,
+arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
+
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
--- index.md
+++ index.md
@@ -1,9 +1,22 @@
 ---
 title: The Windsor Castle
 image: "/assets/img/og-image.png"
-images:
-- "/uploads/gallery-01.jpg"
-- "/uploads/gallery-02.jpg"
+food-images:
+- "/uploads/food-01.jpg"
+- "/uploads/food-02.jpg"
+- "/uploads/food-03.jpg"
+- "/uploads/food-04.jpg"
+- "/uploads/food-05.jpg"
+- "/uploads/food-06.jpg"
+- "/uploads/food-07.jpg"
+- "/uploads/food-08.jpg"
+- "/uploads/food-09.jpg"
+drinks-images:
+- "/uploads/drinks-01.jpg"
+- "/uploads/drinks-02.jpg"
+- "/uploads/drinks-03.jpg"
+- "/uploads/drinks-04.jpg"
+- "/uploads/drinks-05.jpg"
 layout: index
 ---
 
